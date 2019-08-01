6f22eb69b651055efa3dc4f78b6083c68ff114ff
enira <robbert.lambrechts@gmail.com>
2019-08-01 10:27:26 +0200

    ui fix < 196 - place at bottom

--- smb.js
+++ smb.js
@@ -761,10 +761,12 @@ document.getElementById("system_password").addEventListener("keyup", function(ev
   }
 });
 
+cockpit.transport.wait(function() { });
+
+
 // UI fix for versions < 196
 if(parseInt(cockpit.info["version"]) < 196) {
      var elem  = document.getElementById("pagecontent");
      elem.setAttribute("style", "margin-top: 30px;");
 }
 
-cockpit.transport.wait(function() { });
